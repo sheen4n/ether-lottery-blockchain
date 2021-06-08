dc276f76432c6d97ef554a0143643036997f0d3d
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2021-06-08 11:51:38 +0800

    Revert "Create Campaign project"
    This reverts commit b7ac6769c422ccce4f7992816a151b2f64cf2450.

--- deploy.js
+++ deploy.js
@@ -17,7 +17,6 @@ const deploy = async () => {
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' });
 
-  console.log(interface);
   console.log('Contract deployed to', result.options.address);
 };
 
